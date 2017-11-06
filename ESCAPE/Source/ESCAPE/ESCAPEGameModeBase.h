47152c5df8c10ba5c192e2b229ab36a4f421e172
DESKTOP-CTEINEJ\User <lenny5201314@gmail.com>
2017-11-06 10:36:37 +0800

    TEST

---
+++ ESCAPE/Source/ESCAPE/ESCAPEGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ESCAPEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPE_API AESCAPEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
